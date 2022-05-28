ead5aa1ce28f88693090d24dbec0e28100a123a5
pablo-marin-gomez-us <pabmargom3@alum.us.es>
2022-05-28 15:18:15 +0200

    Added tests for update CHIMPUM
    - Added a csv for the tests too

---
+++ src/test/java/acme/testing/inventor/chimpum/InventorChimpumUpdateTest.java
@@ -0,0 +1,69 @@
+package acme.testing.inventor.chimpum;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class InventorChimpumUpdateTest extends TestHarness {
+	
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/chimpum/update.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positive(final int recordIndex, final String pattern, final String title, final String description, final String creationMoment, final String startDate, final String finishDate, final String budget, final String link) {
+		
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "List chimpums");
+		super.checkListingExists();
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("finishDate", finishDate);
+		super.fillInputBoxIn("budget", budget);
+		super.fillInputBoxIn("link", link);
+		
+		super.clickOnSubmit("Update");
+		
+		super.checkNotErrorsExist();
+		super.sortListing(0, "asc");
+		
+		super.clickOnMenu("Inventor", "List chimpums");
+		super.checkListingExists();
+		
+		super.checkColumnHasValue(recordIndex, 1, title);
+		super.checkColumnHasValue(recordIndex, 2, description);
+		super.checkColumnHasValue(recordIndex, 3, startDate);		
+		super.checkColumnHasValue(recordIndex, 4, finishDate);
+		super.checkColumnHasValue(recordIndex, 5, budget);	
+		super.checkColumnHasValue(recordIndex, 6, link);	
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/inventor/chimpum/updateNegative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(20)
+	public void negative(final int recordIndex, final String pattern, final String title, final String description, final String creationMoment, final String startDate, final String finishDate, final String budget, final String link) {
+		
+		super.signIn("inventor1", "inventor1");
+		
+		super.clickOnMenu("Inventor", "List chimpums");
+		super.checkListingExists();
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("description", description);
+		super.fillInputBoxIn("startDate", startDate);
+		super.fillInputBoxIn("finishDate", finishDate);
+		super.fillInputBoxIn("budget", budget);
+		super.fillInputBoxIn("link", link);
+		
+		super.clickOnSubmit("Update");
+		
+		super.checkErrorsExist();
+		
+	}
+}
